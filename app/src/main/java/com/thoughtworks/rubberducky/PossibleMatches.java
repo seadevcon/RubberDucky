9e8d1d3a8229dfb6f82c24da3c7f669f67526e41
Sophie Au <SophieAu@users.noreply.github.com>
2018-09-08 10:44:16 +0200

    Stuff

--- app/src/main/java/com/thoughtworks/rubberducky/PossibleMatches.java
+++ app/src/main/java/com/thoughtworks/rubberducky/PossibleMatches.java
@@ -19,5 +19,7 @@ public class PossibleMatches extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
 
+
+
     }
 }
