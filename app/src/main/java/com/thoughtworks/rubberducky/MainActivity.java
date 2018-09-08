f09b37ee263b5df350859394b9ced84a2c6d42c4
Simon Weiler <sweiler@thoughtworks.com>
2018-09-08 09:45:21 +0200

    add record button

--- app/src/main/java/com/thoughtworks/rubberducky/MainActivity.java
+++ app/src/main/java/com/thoughtworks/rubberducky/MainActivity.java
@@ -2,14 +2,11 @@ package com.thoughtworks.rubberducky;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 import android.widget.Button;
 
 import com.github.florent37.camerafragment.CameraFragment;
@@ -39,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         @SuppressLint("MissingPermission") CameraFragment cameraFragment = CameraFragment.newInstance(new Configuration.Builder().build());
 
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.content, cameraFragment, null)
+                .replace(R.id.camera_view, cameraFragment, null)
                 .commit();
     }
 
